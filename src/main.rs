216b280f3a533db346a1cd91d4990c814eb90f48
Rate980 <xv0nnj0@icloud.com>
2023-01-19 11:53:55 +0900

    add test

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,17 @@
 use from_macro::union_enum;
-union_enum!(Data; u8, u32);
-
-fn main() {}
+// macro_rules! _UnionEnum {
+//     ($name:ident; $($types:ty: $names:ident),* ) => {
+//         enum $name{
+//             $($names($types)),*
+//         }
+//         $(impl From<$types> for $name{
+//             fn from(value: $types) -> Self{
+//                 Self::$names(value)
+//             }
+//         })*
+//     };
+// }
+union_enum! {Data; [String; 3], (u8, u16): B,}
+fn main() {
+    // let a: Data = 2.into();
+}
